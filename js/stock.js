bd38044f59b910a64c9ecb412a1fbbec24b93687
Alvaro Puente <alvaropuentecorrea@gmail.com>
2023-02-18 00:52:29 +0100

    add grid to gallery and swal buy confirm

--- js/stock.js
+++ js/stock.js
@@ -16,13 +16,50 @@ function showStock () {
     })
         .then((response) => response.json())
         .then((articles) => {
+            console.log(articles);
             for (let i = 0; i < articles.length; i++) {
                 const element = document.createElement('div');
+                element.id = 'div-article';
+                element.setAttribute('class', 'text-center');
                 container.appendChild(element);
+                // IMG
                 const imgArticle = document.createElement('img');
                 imgArticle.id = 'img-article';
                 imgArticle.src = 'img/' + articles[i].imagen;
                 element.appendChild(imgArticle);
+                // ARTICLE NAME
+                const articleName = document.createElement('h3');
+                articleName.textContent = articles[i].nombre;
+                element.appendChild(articleName);
+                // PRICE
+                const precio = document.createElement('h4');
+                precio.textContent = articles[i].precio + '€';
+                element.appendChild(precio);
+                // DIV BUTTON
+                const divButton = document.createElement('div');
+                divButton.setAttribute('class', 'div-button');
+                element.appendChild(divButton);
+                // BUTTON
+                const boton = document.createElement('button');
+                boton.textContent = 'comprar';
+                boton.setAttribute('id', articles[i].id);
+                boton.setAttribute('class', 'btn btn-primary mb-2');
+                boton.addEventListener('click', buyArticle);
+                divButton.appendChild(boton);
             }
         });
+}
+// BUY FUCTION
+function buyArticle (e) {
+    const idArticle = e.target.id;
+
+    // eslint-disable-next-line no-undef
+    swal.fire({
+        title: 'COOPbyDWC',
+        text: 'Artículo vendido!',
+        icon: 'success',
+        button: 'Continuar'
+    }).then(function () {
+        window.location = 'stock.html';
+    });
 }
